423353eef2d48b6a477e5ad594c548d0c28cfd28
Pray34 <120999165+Pray34@users.noreply.github.com>
2024-06-03 16:47:43 +0900

    0603_Init

---
+++ Source/ThirdPeronCPP/ThirdPeronCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPeronCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERONCPP_API AThirdPeronCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
